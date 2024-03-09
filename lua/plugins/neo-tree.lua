2883358b3c208cbe89bd7c286b3b53af4ebe4797
Shelton Francisco <ssalicuchepa@technoplus.co.mz>
2024-03-09 16:24:21 +0200

    feat: lsp-config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,18 @@ return {
     "MunifTanjim/nui.nvim",
     "3rd/image.nvim",
   },
-  config = function() 
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+  config = function()
+    vim.keymap.set("n" , "<C-n>", ":Neotree filesystem reveal left toggle<CR>")
+    require("neo-tree").setup({
+      source_selector = {
+        winbar = false,
+        statusline = false
+      },
+      window = {
+        mappings = {
+          ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+        }
+      }
+    })
   end
 }
